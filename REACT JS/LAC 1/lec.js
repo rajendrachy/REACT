9b86cc7a8f495ed13ba76fce939743e9b375f874
rajendrachy <chyrajendra32@gmail.com>
2025-01-07 17:59:21 +0530

    upto lac 3

---
+++ REACT JS/LAC 1/lec.js
@@ -0,0 +1,25 @@
+let cont1 = document.querySelector(".container1");
+let cont2 = document.querySelector(".container2");
+let input = document.querySelector("#in");
+let btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function() {
+ 
+
+
+        cont2.appendChild(input);
+
+   
+
+    cont2.style.backgroundColor = "red";
+
+
+    
+});
+
+let bt = document.querySelector("#bt");
+
+bt.addEventListener("click", function() {
+
+    cont1.append(input);
+});
